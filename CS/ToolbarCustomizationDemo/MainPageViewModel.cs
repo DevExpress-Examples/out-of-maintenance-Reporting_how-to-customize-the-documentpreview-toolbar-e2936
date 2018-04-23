4196bcb1f9af20c9771c09556d804745085dbf84
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ToolbarCustomizationDemo/MainPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows.Input;
+using DevExpress.Xpf.Core.Commands;
+using DevExpress.Xpf.Printing;
+
+namespace ToolbarCustomizationDemo
+{
+    public class MainPageViewModel
+    {
+        readonly ReportPreviewModel previewModel;
+        readonly DelegateCommand<object> createCommand;
+
+        public IDocumentPreviewModel PreviewModel { get { return previewModel; } }
+        public ICommand CreateCommand { get { return createCommand; } }
+
+        public MainPageViewModel()
+        {
+            previewModel = new ReportPreviewModel() { ServiceUri = "../ReportService.svc", ReportTypeName = "ToolbarCustomizationDemo.Web.SampleReport" };
+            createCommand = new DelegateCommand<object>(ExecuteCreateCommand, CanExecuteCreateCommand);
+        }
+
+        bool CanExecuteCreateCommand(object parameter)
+        {
+            return true;
+        }
+
+        void ExecuteCreateCommand(object parameter)
+        {
+            previewModel.CreateDocument();
+        }
+    }
+}
---
+++ VB/ToolbarCustomizationDemo/MainPageViewModel.vb
@@ -0,0 +1,35 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows.Input
+Imports DevExpress.Xpf.Core.Commands
+Imports DevExpress.Xpf.Printing
+
+Namespace ToolbarCustomizationDemo
+	Public Class MainPageViewModel
+		Private ReadOnly previewModel_Renamed As ReportPreviewModel
+		Private ReadOnly createCommand_Renamed As DelegateCommand(Of Object)
+
+		Public ReadOnly Property PreviewModel() As IDocumentPreviewModel
+			Get
+				Return previewModel_Renamed
+			End Get
+		End Property
+		Public ReadOnly Property CreateCommand() As ICommand
+			Get
+				Return createCommand_Renamed
+			End Get
+		End Property
+
+		Public Sub New()
+			previewModel_Renamed = New ReportPreviewModel() With {.ServiceUri = "../ReportService.svc", .ReportTypeName = "ToolbarCustomizationDemo.Web.SampleReport"}
+			createCommand_Renamed = New DelegateCommand(Of Object)(AddressOf ExecuteCreateCommand, AddressOf CanExecuteCreateCommand)
+		End Sub
+
+		Private Function CanExecuteCreateCommand(ByVal parameter As Object) As Boolean
+			Return True
+		End Function
+
+		Private Sub ExecuteCreateCommand(ByVal parameter As Object)
+			previewModel_Renamed.CreateDocument()
+		End Sub
+	End Class
+End Namespace
