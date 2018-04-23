d362f5ebe3eeb30fa89d755a25b51c2d78c28d46
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ToolbarCustomizationDemo/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+using DevExpress.Xpf.Bars;
+// ...
+
+namespace ToolbarCustomizationDemo {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            preview.BarManager.ItemClick += new ItemClickEventHandler(BarManager_ItemClick);
+        }
+
+        void BarManager_ItemClick(object sender, ItemClickEventArgs e) {
+            if (e.Item.Name == "clearButton") {
+                ((MainPageViewModel)DataContext).PreviewModel.Clear();
+            }
+        }
+
+    }
+}
---
+++ VB/ToolbarCustomizationDemo/MainPage.xaml.vb
@@ -0,0 +1,21 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows.Controls
+Imports DevExpress.Xpf.Bars
+' ...
+
+Namespace ToolbarCustomizationDemo
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+			AddHandler preview.BarManager.ItemClick, AddressOf BarManager_ItemClick
+		End Sub
+
+		Private Sub BarManager_ItemClick(ByVal sender As Object, ByVal e As ItemClickEventArgs)
+			If e.Item.Name = "clearButton" Then
+				CType(DataContext, MainPageViewModel).PreviewModel.Clear()
+			End If
+		End Sub
+
+	End Class
+End Namespace
